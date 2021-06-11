999f35dbccfa4831ba12326e425e10a0d4a01ddb
cbabroski <cbabroski@users.noreply.github.com>
2021-06-11 18:19:08 -0400

    Fixed generated header indenting

--- main/maze_generated.h
+++ main/maze_generated.h
@@ -1,21 +1,21 @@
-    /* THIS IS A GENERATED FILE, DO NOT EDIT MANUALLY. */
+/* THIS IS A GENERATED FILE, DO NOT EDIT MANUALLY. */
 
-    #ifndef MAZE_GENERATED_H
-    #define MAZE_GENERATED_H
+#ifndef MAZE_GENERATED_H
+#define MAZE_GENERATED_H
 
-    #define GEN_MAX_NUM_WALLS (15)
-    #define GEN_BALL_RADIUS (7)
-    #define GEN_DISPLAY_HEIGHT (240)
-    #define GEN_DISPLAY_WIDTH (320)
+#define GEN_MAX_NUM_WALLS (15)
+#define GEN_BALL_RADIUS (7)
+#define GEN_DISPLAY_HEIGHT (240)
+#define GEN_DISPLAY_WIDTH (320)
 
-    /* Initial ball position (ball_t) */
-    const char g_start_ball[] = {
+/* Initial ball position (ball_t) */
+const char g_start_ball[] = {
 	0x7, 0x0, 0x16, 0x0, 0x7, 0x0, 0x16, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
 	0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
 };
 
-    /* Generated maze (maze_t) */
-    const char g_generated_maze[] = {
+/* Generated maze (maze_t) */
+const char g_generated_maze[] = {
 	0x0, 0x0, 0x1e, 0x0, 0xf0, 0x0, 0x1e, 0x0, 0x3c, 0x0, 0x5a, 0x0, 0xb4,
 	0x0, 0x5a, 0x0, 0x78, 0x0, 0x87, 0x0, 0xb4, 0x0, 0x87, 0x0, 0x3c, 0x0,
 	0xb4, 0x0, 0xf0, 0x0, 0xb4, 0x0, 0x3c, 0x0, 0x5a, 0x0, 0x3c, 0x0,
@@ -28,5 +28,4 @@
 	0x0, 0x0, 0x8, 0x0
 };
 
-    #endif /* MAZE_GENERATED_H */
-    
+#endif /* MAZE_GENERATED_H */
